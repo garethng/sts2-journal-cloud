09bd2bf5725c299851a2d0451d831a858f36d996
Gareth Ng <wu.qingzhe@outlook.com>
2026-03-17 19:02:16 +0800

    Initial commit

---
+++ data/game-index.js
@@ -0,0 +1,64 @@
+// Game Index Mapping
+// Generated from spire-codex data
+
+const GAME_INDEX = {
+    // Monsters array (index = monster ID)
+    monsters: [{"id": "ARCHITECT", "name": "建筑师"}, {"id": "ASSASSIN_RUBY_RAIDER", "name": "劫掠者刺客"}, {"id": "AXEBOT", "name": "巨斧机器人"}, {"id": "AXE_RUBY_RAIDER", "name": "劫掠者斧手"}, {"id": "BATTLE_FRIEND_V1", "name": "战斗好伙伴V1.0"}, {"id": "BATTLE_FRIEND_V2", "name": "战斗好伙伴V2.0"}, {"id": "BATTLE_FRIEND_V3", "name": "战斗好伙伴V3.0"}, {"id": "BOWLBUG_EGG", "name": "盛碗虫（卵）"}, {"id": "BOWLBUG_NECTAR", "name": "盛碗虫（蜜）"}, {"id": "BOWLBUG_ROCK", "name": "盛碗虫（石）"}, {"id": "BOWLBUG_SILK", "name": "盛碗虫（丝）"}, {"id": "BRUTE_RUBY_RAIDER", "name": "劫掠者暴徒"}, {"id": "BYGONE_EFFIGY", "name": "旧日雕像"}, {"id": "BYRDONIS", "name": "多尼斯异鸟"}, {"id": "BYRDPIP", "name": "异鸟宝宝"}, {"id": "CALCIFIED_CULTIST", "name": "钙化邪教徒"}, {"id": "CEREMONIAL_BEAST", "name": "仪式兽"}, {"id": "CHOMPER", "name": "啃咬机"}, {"id": "CORPSE_SLUG", "name": "噬尸蛞蝓"}, {"id": "CROSSBOW_RUBY_RAIDER", "name": "劫掠者弩手"}, {"id": "CRUSHER", "name": "碾碎爪"}, {"id": "CUBEX_CONSTRUCT", "name": "立柱构造体"}, {"id": "DAMP_CULTIST", "name": "潮湿邪教徒"}, {"id": "DECIMILLIPEDE_SEGMENT", "name": "残杀千足虫"}, {"id": "DEVOTED_SCULPTOR", "name": "虔诚雕刻师"}, {"id": "DOOR", "name": "门扉"}, {"id": "DOORMAKER", "name": "门扉缔造者"}, {"id": "ENTOMANCER", "name": "蜂群术士"}, {"id": "EXOSKELETON", "name": "外骨骼虫"}, {"id": "EYE_WITH_TEETH", "name": "利齿之眼"}, {"id": "FABRICATOR", "name": "组装师"}, {"id": "FAT_GREMLIN", "name": "胖地精"}, {"id": "FLAIL_KNIGHT", "name": "连枷骑士"}, {"id": "FLYCONID", "name": "飞蝇菌子"}, {"id": "FOGMOG", "name": "雾菇"}, {"id": "FOSSIL_STALKER", "name": "化石追踪者"}, {"id": "FROG_KNIGHT", "name": "青蛙骑士"}, {"id": "FUZZY_WURM_CRAWLER", "name": "毛绒伏地虫"}, {"id": "GAS_BOMB", "name": "气态炸弹"}, {"id": "GLOBE_HEAD", "name": "电球头"}, {"id": "GREMLIN_MERC", "name": "地精佣兵"}, {"id": "GUARDBOT", "name": "守护机器人"}, {"id": "HAUNTED_SHIP", "name": "幽灵船"}, {"id": "HUNTER_KILLER", "name": "猎人杀手"}, {"id": "INFESTED_PRISM", "name": "感染棱柱"}, {"id": "INKLET", "name": "墨宝"}, {"id": "KIN_FOLLOWER", "name": "同族信徒"}, {"id": "KIN_PRIEST", "name": "同族神官"}, {"id": "KNOWLEDGE_DEMON", "name": "知识恶魔"}, {"id": "LAGAVULIN_MATRIARCH", "name": "乐加维林族母"}, {"id": "LEAF_SLIME_M", "name": "树叶史莱姆（中）"}, {"id": "LEAF_SLIME_S", "name": "树叶史莱姆（小）"}, {"id": "LIVING_FOG", "name": "活雾"}, {"id": "LIVING_SHIELD", "name": "活体盾"}, {"id": "LOUSE_PROGENITOR", "name": "虱虫之祖"}, {"id": "MAGI_KNIGHT", "name": "魔法骑士"}, {"id": "MAWLER", "name": "蛮兽"}, {"id": "MECHA_KNIGHT", "name": "机甲骑士"}, {"id": "MYTE", "name": "异螨"}, {"id": "NIBBIT", "name": "小啃兽"}, {"id": "NOISEBOT", "name": "噪音机器人"}, {"id": "OSTY", "name": "奥斯提"}, {"id": "OVICOPTER", "name": "直飞产卵虫"}, {"id": "OWL_MAGISTRATE", "name": "猫头鹰法官"}, {"id": "PAELS_LEGION", "name": "佩尔的士兵"}, {"id": "PARAFRIGHT", "name": "寄生惧魔"}, {"id": "PHANTASMAL_GARDENER", "name": "花园幽灵鳗"}, {"id": "PHROG_PARASITE", "name": "异蛙寄生虫"}, {"id": "PUNCH_CONSTRUCT", "name": "拳击构装体"}, {"id": "QUEEN", "name": "女王"}, {"id": "ROCKET", "name": "火箭"}, {"id": "SCROLL_OF_BITING", "name": "咬人卷轴"}, {"id": "SEAPUNK", "name": "海洋混混"}, {"id": "SEWER_CLAM", "name": "下水道蚌"}, {"id": "SHRINKER_BEETLE", "name": "缩小甲虫"}, {"id": "SKULKING_COLONY", "name": "鬼祟珊瑚群"}, {"id": "SLIMED_BERSERKER", "name": "史莱姆狂战士"}, {"id": "SLITHERING_STRANGLER", "name": "蛇行扼杀者"}, {"id": "SLUDGE_SPINNER", "name": "淤泥旋螺"}, {"id": "SLUMBERING_BEETLE", "name": "熟睡甲虫"}, {"id": "SNAPPING_JAXFRUIT", "name": "闪光贾克斯果"}, {"id": "SNEAKY_GREMLIN", "name": "卑鄙地精"}, {"id": "SOUL_FYSH", "name": "灵魂异鱼"}, {"id": "SOUL_NEXUS", "name": "灵魂枢纽"}, {"id": "SPECTRAL_KNIGHT", "name": "幽灵骑士"}, {"id": "SPINY_TOAD", "name": "棘刺蟾蜍"}, {"id": "STABBOT", "name": "戳刺机器人"}, {"id": "TERROR_EEL", "name": "骇鳗"}, {"id": "TEST_SUBJECT", "name": "实验体 #C{Count}"}, {"id": "THE_ADVERSARY_MK_ONE", "name": "对手1型"}, {"id": "THE_ADVERSARY_MK_THREE", "name": "对手3型"}, {"id": "THE_ADVERSARY_MK_TWO", "name": "对手2型"}, {"id": "THE_FORGOTTEN", "name": "遗忘之物"}, {"id": "THE_INSATIABLE", "name": "无厌沙虫"}, {"id": "THE_LOST", "name": "失落之物"}, {"id": "THE_OBSCURA", "name": "胧光怪"}, {"id": "THIEVING_HOPPER", "name": "偷窃草蜢"}, {"id": "TOADPOLE", "name": "蟾蜍蝌蚪"}, {"id": "TORCH_HEAD_AMALGAM", "name": "火炬头聚合体"}, {"id": "TOUGH_EGG", "name": "结实的卵"}, {"id": "TRACKER_RUBY_RAIDER", "name": "劫掠者追踪手"}, {"id": "TUNNELER", "name": "地道虫"}, {"id": "TURRET_OPERATOR", "name": "高塔炮手"}, {"id": "TWIG_SLIME_M", "name": "树枝史莱姆（中）"}, {"id": "TWIG_SLIME_S", "name": "树枝史莱姆（小）"}, {"id": "TWO_TAILED_RAT", "name": "双尾鼠"}, {"id": "VANTOM", "name": "墨影幻灵"}, {"id": "VINE_SHAMBLER", "name": "藤蔓蹒跚者"}, {"id": "WATERFALL_GIANT", "name": "瀑布巨兽"}, {"id": "WRIGGLER", "name": "扭动虫"}, {"id": "ZAPBOT", "name": "电击机器人"}],
+    
+    // Events array (index = event ID)
+    events: [{"id": "ABYSSAL_BATHS", "name": "深渊浴场"}, {"id": "AMALGAMATOR", "name": "熔合者"}, {"id": "AROMA_OF_CHAOS", "name": "混沌芳香"}, {"id": "BATTLEWORN_DUMMY", "name": "战痕累累的训练假人"}, {"id": "BRAIN_LEECH", "name": "脑蛭"}, {"id": "BUGSLAYER", "name": "害虫杀手"}, {"id": "BYRDONIS_NEST", "name": "多尼斯异鸟巢"}, {"id": "COLORFUL_PHILOSOPHERS", "name": "色彩哲学家"}, {"id": "COLOSSAL_FLOWER", "name": "巨大花卉"}, {"id": "CRYSTAL_SPHERE", "name": "水晶球"}, {"id": "DARV", "name": "达弗"}, {"id": "DENSE_VEGETATION", "name": "茂密的植被"}, {"id": "DOLL_ROOM", "name": "玩偶室"}, {"id": "DOORS_OF_LIGHT_AND_DARK", "name": "光与暗的门扉"}, {"id": "DROWNING_BEACON", "name": "淹水灯塔"}, {"id": "ENDLESS_CONVEYOR", "name": "无尽传送带"}, {"id": "FAKE_MERCHANT", "name": "商人？？？"}, {"id": "FIELD_OF_MAN_SIZED_HOLES", "name": "人形洞穴之地"}, {"id": "GRAVE_OF_THE_FORGOTTEN", "name": "遗忘之墓"}, {"id": "HUNGRY_FOR_MUSHROOMS", "name": "蘑菇饥渴"}, {"id": "INFESTED_AUTOMATON", "name": "被寄生的自动机械"}, {"id": "JUNGLE_MAZE_ADVENTURE", "name": "丛林迷宫奇遇"}, {"id": "LOST_WISP", "name": "迷失鬼火"}, {"id": "LUMINOUS_CHOIR", "name": "冷光合唱团"}, {"id": "MORPHIC_GROVE", "name": "变形灵林谷"}, {"id": "NEOW", "name": "涅奥"}, {"id": "NONUPEIPE", "name": "诺奴佩普"}, {"id": "OROBAS", "name": "欧洛巴斯"}, {"id": "PAEL", "name": "佩尔"}, {"id": "POTION_COURIER", "name": "药水快递员"}, {"id": "PUNCH_OFF", "name": "重拳出击"}, {"id": "RANWID_THE_ELDER", "name": "长者兰伟德"}, {"id": "REFLECTIONS", "name": "镜中倒影  影倒中镜"}, {"id": "RELIC_TRADER", "name": "遗物交换商"}, {"id": "ROOM_FULL_OF_CHEESE", "name": "满屋芝士"}, {"id": "ROUND_TEA_PARTY", "name": "圆桌茶会"}, {"id": "SAPPHIRE_SEED", "name": "蓝宝石种子"}, {"id": "SELF_HELP_BOOK", "name": "自助指南"}, {"id": "SLIPPERY_BRIDGE", "name": "滑脚木桥"}, {"id": "SPIRALING_WHIRLPOOL", "name": "螺旋漩涡"}, {"id": "SPIRIT_GRAFTER", "name": "灵魂嫁接者"}, {"id": "STONE_OF_ALL_TIME", "name": "永恒之石"}, {"id": "SUNKEN_STATUE", "name": "沉没雕像"}, {"id": "SUNKEN_TREASURY", "name": "淹水金库"}, {"id": "SYMBIOTE", "name": "共生体"}, {"id": "TABLET_OF_TRUTH", "name": "真理石板"}, {"id": "TANX", "name": "坦克斯"}, {"id": "TEA_MASTER", "name": "茶艺大师"}, {"id": "TEZCATARA", "name": "特兹卡塔拉"}, {"id": "THE_ARCHITECT", "name": "建筑师"}, {"id": "THE_FUTURE_OF_POTIONS", "name": "药水的未来？"}, {"id": "THE_LANTERN_KEY", "name": "灯火钥匙"}, {"id": "THE_LEGENDS_WERE_TRUE", "name": "传说是真的"}, {"id": "THIS_OR_THAT", "name": "这个还是那个？"}, {"id": "TINKER_TIME", "name": "打造时间"}, {"id": "TRASH_HEAP", "name": "垃圾堆"}, {"id": "TRIAL", "name": "审判"}, {"id": "UNREST_SITE", "name": "无休之处"}, {"id": "VAKUU", "name": "瓦库"}, {"id": "WAR_HISTORIAN_REPY", "name": "战史学家 付袭"}, {"id": "WATERLOGGED_SCRIPTORIUM", "name": "水漫缮写室"}, {"id": "WELCOME_TO_WONGOS", "name": "欢迎来到旺购百货"}, {"id": "WELLSPRING", "name": "泉水"}, {"id": "WHISPERING_HOLLOW", "name": "低语空谷"}, {"id": "WOOD_CARVINGS", "name": "木雕"}, {"id": "ZEN_WEAVER", "name": "修禅织网者"}],
+    
+    // Special rooms
+    specialRooms: [
+        {id: 'SHOP', name: '商店'},
+        {id: 'TREASURE', name: '宝箱'},
+        {id: 'REST', name: '休息处'},
+        {id: 'EVENT', name: '事件'},
+        {id: 'ANCIENT', name: '远古遗迹'},
+        {id: 'UNKNOWN', name: '未知'}
+    ]
+};
+
+// Build reverse lookup
+const monsterNameToId = {};
+const monsterIdToName = {};
+GAME_INDEX.monsters.forEach((m, idx) => {
+    monsterNameToId[m.name] = idx;
+    monsterIdToName[m.id] = idx;
+});
+
+const eventNameToId = {};
+const eventIdToName = {};
+GAME_INDEX.events.forEach((e, idx) => {
+    eventNameToId[e.name] = idx;
+    eventIdToName[e.id] = idx;
+});
+
+// Lookup functions
+function getMonsterIdByName(name) {
+    if (monsterNameToId[name] !== undefined) return monsterNameToId[name];
+    const cleanName = name.replace(/[（(].*?[)）]/g, '').trim();
+    if (monsterNameToId[cleanName] !== undefined) return monsterNameToId[cleanName];
+    const idFormat = name.toUpperCase().replace(/\s+/g, '_');
+    if (monsterIdToName[idFormat] !== undefined) return monsterIdToName[idFormat];
+    if (idFormat.endsWith('S')) {
+        const singular = idFormat.slice(0, -1);
+        if (monsterIdToName[singular] !== undefined) return monsterIdToName[singular];
+    }
+    return 0;
+}
+
+function getMonsterById(id) {
+    return GAME_INDEX.monsters[id] || GAME_INDEX.monsters[0];
+}
+
+function getEventIdByName(name) {
+    if (eventNameToId[name] !== undefined) return eventNameToId[name];
+    const idFormat = name.toUpperCase().replace(/\s+/g, '_');
+    if (eventIdToName[idFormat] !== undefined) return eventIdToName[idFormat];
+    return 0;
+}
+
+function getEventById(id) {
+    return GAME_INDEX.events[id] || GAME_INDEX.events[0];
+}
